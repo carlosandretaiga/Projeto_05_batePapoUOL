68159ca2560d45a13c6fb78f6ffd95d3623154a0
Carlos André Lima de Matos <carlosandretaiga@gmail.com>
2022-04-16 22:03:17 -0300

    Recebendo mensagens do chat e testando envio

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,7 @@
 let messageChat = []; 
+let nameUser; 
 
-
+getMessages();
 
  //Recebendo as mensagens do chat
 function getMessages () {
@@ -14,25 +15,59 @@ function getMessages () {
 }
 
 //Função de carregar dados de mensagens que já estão na API
-function loadMessages () {
+function loadMessages (response) {
     messageChat = response.data; 
     //aqui colocar função para renderizar mensagens
+    renderMessages(); 
 
 }
 
 
 
 //Aqui criar função para renderizar mensagens
+function renderMessages () {
+    const listMessages = document.querySelector(".box-message"); 
+    listMessages.innerHTML = ""; 
+
+    for (let i = 0; i < messageChat.length; i++) {
+        listMessages.innerHTML += `
+        <div class="status-messages">
+
+                <div class="status-messages-hour">
+                    (${messageChat[i].time})
+                </div>
+
+                <div class="status-messages-name">
+                ${messageChat[i].from}
+                </div>
+
+                <div class="status-messages-to">
+                ${messageChat[i].to}
+                </div>
+
+                <div class="status-messages-message">
+                ${messageChat[i].text}
+                </div>
+
+        </div>
+        
+        `;
+    }
+
+
+
+}
+
+//se type === "status" => deverá escolher um layout .status-messages
+//se type === "message" => deverá escolher um layout .normal-messages
+//se type === "private_message" => deverá escolher layout .reserved-messages
 
 
 
 
 
 
-
-
-
-//Processar error 
+//Processar error entrar na sala 
 function errorProcess (error) {
     //console.log(error.response); 
 
@@ -55,12 +90,12 @@ function errorProcess (error) {
 
 //função modelo para liberar página de mensagens
 function enterChat () {
-    const name = document.querySelector(".name-user").value; 
-    console.log(name); 
+    nameUser = document.querySelector(".name-user").value; 
+    console.log(nameUser); 
 
     //objeto para entrada de novo usuário no chat
     const newUser = {
-        name: name, 
+        name: nameUser, 
     }; 
 
     const promise = axios.post(
@@ -75,7 +110,63 @@ function enterChat () {
     promise.catch(errorProcess); 
 }
 
-enterChat (); 
+enterChat(); 
+
+
+//Manter conexão 
+function keepConnection () {
+
+    //objeto para entrada de novo usuário no chat
+    const newUser = {
+        name: nameUser, 
+    }; 
+
+    const promise = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/status",
+        newUser
+    ); 
+
+    //Executar quando a requisição for resolvida com sucesso
+    promise.then(); 
+
+}
+
+setTimeout(keepConnection, 3000); 
+
+
+
+//Enviando mensagem para todos 
+function sendMessage () {
+    const message = document.querySelector(".send-message").value; 
+
+    const newMessage = {
+        from: nameUser, 
+        to: " ",
+        text: message, 
+        type: "message", 
+    }; 
+
+    const promise = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/messages",
+         newMessage
+    ); 
+    
+    //Executar quando a requisição for resolvida com sucesso
+    promise.then(getMessages); 
+
+    //Executar quando a requisição for resolvida com falha
+    //promise.catch()
+
+
+
+}
+
+
+
+
+
+
+
 
 
 
